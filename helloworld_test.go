e8b5e38d920c556a3c6c6e17cb9bfd71e12f0680
Andrew <andrew.cho1992@gmail.com>
2022-11-13 16:36:09 +1100

    Refactor code
    - Handles default case
    - Test code refactored to follow DRY

--- helloworld_test.go
+++ helloworld_test.go
@@ -3,9 +3,28 @@ package main
 import "testing"
 
 func TestHelloAndrew(test *testing.T) {
-	got := Hello("Andrew!")
-	want := "Hello, Andrew!"
+	test.Run("saying hello to people", func(test *testing.T) {
+		got := Hello("Andrew!")
+		want := "Hello, Andrew!"
 
+		// if got != want {
+		// 	test.Errorf("got %q want %q", got, want)
+		// }
+
+		// ** We move the above code into its own function ** :
+
+		assertCorrectMessage(test, got, want)
+	})
+	test.Run("say 'Hello, World' when an empty string is supplied", func(test *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+
+		assertCorrectMessage(test, got, want)
+	})
+}
+
+func assertCorrectMessage(test testing.TB, got, want string) {
+	test.Helper()
 	if got != want {
 		test.Errorf("got %q want %q", got, want)
 	}
